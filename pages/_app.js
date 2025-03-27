e903637909470fc988d34d6c8963fe135b24b66d
janMagnusHeimann <jan.heimann@tum.de>
2025-03-27 13:08:56 +0100

    favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import '../styles/global.css';
 import { ThemeProvider } from 'next-themes';
+import Head from 'next/head'; // Import Head from next/head
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" /> {/* Add favicon link */}
+      </Head>
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
 
